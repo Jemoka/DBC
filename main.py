45367c787f6ee5209ca9ce6b93a0e704d699f34a
jemoka <hliu@shabang.cf>
2022-07-07 11:26:31 -0700

    updated main training params

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cp
 # initialize the model
 CONFIG = {
     "model": "nghuyong/ernie-2.0-en",
-    "batch_size": 4,
+    "batch_size": 72,
     "epochs": 3,
     "lr": 1e-5,
     "max_length": 60
