ca9e9f453ac2a2fc76ff3985d634ed1bdb574639
jemoka <hliu@shabang.cf>
2022-07-11 10:34:06 -0700

    change params

--- validate.py
+++ validate.py
@@ -21,23 +21,20 @@ from tqdm import tqdm
 # pathlib
 from pathlib import Path
 
-# intepretation tools
-from transformers_interpret import SequenceClassificationExplainer
-
 # our utils
-from util import predict_on_sample, eval_model_on_batch
+from util import eval_model_on_batch
 
 # initialize the device
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 TOKENIZER = "./models/royal-pond-21"
-MODEL = "./models/royal-pond-21"
+MODEL = "./models/dummy-LC6tiW5WWr2jew5wbwWkRG"
 MAX_LENGTH = 60
 WINDOW_SIZE = 5
 
 #############################
 
 # Load the current dataset, which is pitt-7-4
-df = pd.read_pickle("./data/transcripts_pauses/alignedpitt-7-8-windowed.bat")
+df = pd.read_pickle("./data/transcripts_pauses/alignedpitt-7-8-flucalc-windowed.bat")
 
 # Get the testing data
 testing_data = df[df["split"] == "test"]
@@ -53,7 +50,6 @@ testing_data = testing_data.reset_index(drop=True)
 # Epic. Let's load our models.
 tokenizer = BertTokenizer.from_pretrained(TOKENIZER)
 model = torch.load(os.path.join(MODEL, "model.bin")).to(DEVICE)
-explainer = SequenceClassificationExplainer(model.base_model, tokenizer)
 
 model.eval()
 
